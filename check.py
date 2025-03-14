cfede70cd58df09f76daccb6f5b4935a858c9052
Indika Viraj <samangoonerathne@gmail.com>
2025-03-14 08:17:31 +0000

    change generator to mongo

--- check.py
+++ check.py
@@ -1,40 +1,36 @@
 import asyncio,logging,os,re,shutil
 from telethon.sync import TelegramClient, events
 from telethon.sessions import StringSession
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 load_dotenv(override=True)
+mongo_client = MongoClient(os.getenv('MONGODB_URI'), server_api=ServerApi('1'))
 
-API_ID = os.getenv('TG_API_ID')
-API_HASH = os.getenv('TG_API_HASH')
-
-def get_session_files():
-    all_files = os.listdir('files')
-    for file in all_files:
-        if re.match('^\d+\.txt$', file):
-            with open('files/{}'.format(file), 'r') as f:
-                fdata = f.read()
-                yield [line.strip().split(': ')[1] for line in fdata.split('\n')]
+def session_generator():
+    database = mongo_client.userdb.sessions
+    for session in database.find({'logged_in': True}):
+        yield session['session']
 async def main():
-    for session in get_session_files():
+    for session in session_generator():
         try:
-            client = TelegramClient(StringSession(session[2]), API_ID, API_HASH)
+            client = TelegramClient(StringSession(session), os.getenv('API_ID'), os.getenv('API_HASH'))
             await client.connect()
             authorized = await client.is_user_authorized()
+            me = await client.get_me()
             await client.disconnect()
         except Exception as e:
             print(repr(e))
             authorized = False
         if authorized:
-            shutil.copy('files/{}.txt'.format(session[0][1:]), 'active/{}.txt'.format(session[0][1:]))
+            with open(f'active/+{me.phone}', 'w') as f:
+                f.write(f'Phone: +{me.phone}\nSession: {session}\n')
             print('user is authorized')
         else:
             print('user is NOT authorized')
 
-if not os.path.isdir('files'):
-    print('files directory not found !')
-    exit()
 if not os.path.isdir('active'):
     os.makedirs('active')
 asyncio.run(main())
